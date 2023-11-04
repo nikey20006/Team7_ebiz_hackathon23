9c7d7a7e85ae1feb13d97b98f40bbcf800c2d96c
j_soup <nikey2000@ajou.ac.kr>
2023-11-04 19:10:14 +0900

    Update 2-4.sql

--- 2-4.sql
+++ 2-4.sql
@@ -1,7 +1,7 @@
 SELECT 
-    country AS 'Country',
-    COUNT(*) AS 'Number of Customers',
-    AVG(age) AS 'Average Age'
+    country AS '나라',
+    COUNT(*) AS '고객수',
+    AVG(age) AS '평균나이'
 FROM Customers
 GROUP BY country
 ORDER BY AVG(age) DESC;
